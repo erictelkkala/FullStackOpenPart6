c8b5aaa578b6c65e520a236d3dfda78c982c35f5
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-08-15 14:41:43 +0300

    Tasks 6.1 and 6.2

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -8,13 +8,28 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return {
+        ...state,
+        good: state.good + 1
+      }
     case 'OK':
-      return state
+      return {
+        ...state,
+        ok: state.ok + 1
+      }
     case 'BAD':
-      return state
+      return {
+        ...state,
+        bad: state.bad + 1
+      }
     case 'ZERO':
-      return state
+      return {
+        ...state,
+        // Reset the counters to zero
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
     default: return state
   }
   
--- unicafe-redux/src/reducer.test.js
+++ unicafe-redux/src/reducer.test.js
@@ -9,7 +9,6 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
     const action = {
       type: 'DO_NOTHING'
     }
@@ -28,6 +27,58 @@ describe('unicafe reducer', () => {
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('States can be reset', () => {
+
+    const action = {
+      type: 'ZERO'
+    }
+    // Define the state to not be all zeroes, since we need to reset it
+    const state = {
+      good: 1,
+      ok: 1,
+      bad: 1
+    }
+
+    deepFreeze(state)
+    
+    const newState2 = counterReducer(state, action)
+    expect(newState2).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
