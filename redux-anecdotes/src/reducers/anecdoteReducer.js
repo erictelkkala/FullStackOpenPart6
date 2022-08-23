c08482488b79fb7ca820497491b25264a2b48919
erictelkkala <eric.telkkala@gmail.com>
2022-08-23 14:36:58 +0300

    Task 6.15

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const initialState = []
 
@@ -25,4 +26,13 @@ const anecdoteSlice = createSlice({
 })
 
 export const { vote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+// Async action creators
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -2,8 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const store = configureStore({
   reducer: {
@@ -12,10 +10,5 @@ const store = configureStore({
     filter: filterReducer,
   },
 })
-
-anecdoteService.getAll().then((anecdotes) => {
-  store.dispatch(setAnecdotes(anecdotes))
-})
-
 
 export default store
