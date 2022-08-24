210388549c3b69284af4c08f815ff58142619d55
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-08-24 19:27:16 +0300

    Task 6.21 => part 6d => section 6 complete

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -4,10 +4,16 @@ const notificationSlice = createSlice({
   name: 'notification',
   initialState: {
     message: '',
+    timeoutID: 0,
   },
   reducers: {
     setNotification: (state, action) => {
       state.message = action.payload.message
+    },
+    setTimeoutID: (state, action) => {
+      // Clear the previous timeout if there is one
+      clearTimeout(state.timeoutID)
+      state.timeoutID = action.payload.timeoutID
     },
   },
 })
@@ -16,13 +22,26 @@ export const setNewNotification = (message, time) => {
   // Convert the time from seconds to milliseconds
   const timeInMilliseconds = time * 1000
 
+  let timeoutID = 0
+
+  function setTimeoutMessage(dispatch) {
+    // Set the timeoutID variable to the ID of the timeout
+    // And clear the message and timeoutID variables after the timeout
+    timeoutID = setTimeout(() => {
+      dispatch(setNotification({ message: '' }))
+      dispatch(setTimeoutID({ timeoutID: 0 }))
+    }, timeInMilliseconds)
+    // This dispatch is called after the timeout has been set
+    // It is used to set the timeoutID state variable to allow for clearing the timeout
+    dispatch(setTimeoutID({ timeoutID: timeoutID }))
+  }
+
   return async (dispatch) => {
     await dispatch(setNotification({ message }))
-    setTimeout(() => {
-      dispatch(setNotification({ message: '' }))
-    }, timeInMilliseconds)
+    // Call the setTimeoutMessage function
+    setTimeoutMessage(dispatch)
   }
 }
 
-export const { setNotification } = notificationSlice.actions
+export const { setNotification, setTimeoutID } = notificationSlice.actions
 export default notificationSlice.reducer
