77edfa0bc58205ec0658033ed5592d40c79537be
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-08-24 18:48:32 +0300

    Task 6.19

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -1,13 +1,16 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notification)
+const Notification = (props) => {
+  // Get the notification message from the props
+  const notification = props.notification
   console.log('Notification: ', notification)
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
   }
+
   // Only show the notification if the message if not empty
   if (notification.message === '' || notification.message === null) {
     return null
@@ -15,4 +18,13 @@ const Notification = () => {
   return <div style={style}>{notification.message}</div>
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+  }
+}
+
+// Connect the state to the props and export the connected component
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
